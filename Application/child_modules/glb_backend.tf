e8c0b93148a87b9838e0330a5451e87a9af60958
guptatrng <79828566+guptatrng@users.noreply.github.com>
2025-04-20 14:45:34 +0530

    Update glb_backend.tf

--- Application/child_modules/glb_backend.tf
+++ Application/child_modules/glb_backend.tf
@@ -68,6 +68,7 @@ resource "google_compute_firewall" "nginx_firewall" {
 resource "google_compute_region_autoscaler" "nginx_instance_group_autoscaler" {
   name = var.instance_group_autoscaler_name
   target = google_compute_region_instance_group_manager.nginx_instance_group.id
+  region = var.region
   autoscaling_policy {
     max_replicas = 3
     min_replicas = 1
